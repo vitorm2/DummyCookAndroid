7b3ed458507b59a8b2e98ec181b852edcfa178a5
Vitor Demenighi <vitor_m2@hotmail.com>
2018-07-21 20:28:14 -0300

    Details Recipe View - Iniciada

--- app/src/main/java/com/example/vitor/dummycook/DetailsActivity.java
+++ app/src/main/java/com/example/vitor/dummycook/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.example.vitor.dummycook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -11,9 +13,16 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+
         Pessoa user = (Pessoa) getIntent().getSerializableExtra("pessoa");
 
-        TextView mText = (TextView) findViewById(R.id.texto1);
+        TextView mText = (TextView) findViewById(R.id.text_recipeName);
+        TextView texto2 = (TextView) findViewById(R.id.text_requiriments);
+        TextView texto3 = (TextView) findViewById(R.id.text_ingredients);
+        ImageView imageRecipe = (ImageView) findViewById(R.id.image_recipe);
+        imageRecipe.setClipToOutline(true);
         mText.setText(user.getName());
+        texto2.setText(user.getCity());
+        texto3.setText(user.getDescription());
     }
 }
